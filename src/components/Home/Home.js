ec1a9959fdbf06d4918db023c35955d815bdc524
Baiel Muratbekov <bajelmuratbekov044@gmail.com>
2022-10-28 21:11:11 +0600

    add new components

---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Grid from '../Grid/Grid'
+import Nav from '../Nav/Nav'
+
+export default function Home() {
+  return (
+    <div>
+        <Nav/>
+        <Grid/>
+    </div>
+  )
+}
