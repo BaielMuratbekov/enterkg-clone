358502c33f9c1ab096fba81a6d8feb224e3cc3a5
Baiel Muratbekov <bajelmuratbekov044@gmail.com>
2022-10-28 22:58:39 +0600

    add new data NavData

---
+++ src/data/NavData.js
@@ -0,0 +1,79 @@
+import { GiWashingMachine } from "react-icons/gi";
+import { BsPhone } from "react-icons/bs";
+import { BsDisplay } from "react-icons/bs";
+import { MdComputer } from "react-icons/md";
+import { BsPrinter } from "react-icons/bs";
+import { FaUmbrellaBeach } from "react-icons/fa";
+import { BsTools } from "react-icons/bs";
+import { FaPaintRoller } from "react-icons/fa";
+import { AiOutlineHome } from "react-icons/ai";
+import { GiCartwheel } from "react-icons/gi";
+import { FiHeadphones } from "react-icons/fi";
+export const NavData = [
+  {
+    title: "Бытовая техника",
+    subtitle: "fkmk",
+    path: "/",
+    icon: <GiWashingMachine />,
+  },
+  {
+    title: "Смартфоны и гаджеты",
+    subtitle: "jnn",
+    path: "/",
+    icon: <BsPhone />,
+  },
+  {
+    title: "ТВ и мультимедиа",
+    subtitle: "mlkl",
+    path: "/",
+    icon: <BsDisplay />,
+  },
+  {
+    title: "Компьютеры",
+    subtitle: "",
+    path: "/",
+    icon: <MdComputer />,
+  },
+  {
+    title: "Офис и сеть",
+    subtitle: "",
+    path: "/",
+    icon: <BsPrinter />,
+  },
+  {
+    title: "Отдых и развлечения",
+    subtitle: "",
+    path: "/",
+    icon: <FaUmbrellaBeach />,
+  },
+  {
+    title: "Инструменты",
+    subtitle: "",
+    path: "/",
+    icon: <BsTools />,
+  },
+  {
+    title: "Строительство и ремонт",
+    subtitle: "",
+    path: "/",
+    icon: <FaPaintRoller />,
+  },
+  {
+    title: "Дом, декор и кухня ",
+    subtitle: "",
+    path: "/",
+    icon: <AiOutlineHome />,
+  },
+  {
+    title: "Автотовары",
+    subtitle: "",
+    path: "/",
+    icon: <GiCartwheel />,
+  },
+  {
+    title: "Аксессуары и услуги",
+    subtitle: "",
+    path: "/",
+    icon: <FiHeadphones />,
+  },
+];
