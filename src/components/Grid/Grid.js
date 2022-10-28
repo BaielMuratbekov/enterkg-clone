3741702f0b9af9035dbd84347fd7abee5fa9810e
Baiel Muratbekov <bajelmuratbekov044@gmail.com>
2022-10-28 23:24:46 +0600

    update grid

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -1,14 +1,19 @@
 import React from "react";
 import MobileMenu from "../MobileMenu/MobileMenu";
-
 export default function Grid() {
   return (
-    <div className="grid grid-rows-3 grid-flow-col gap-4  container ">
-      <div className="row-span-3 shadow-2xl rounded-md max-w-max">
-        <MobileMenu/>
+    <div className=" container flex  ">
+      <div className=" shadow-2xl rounded-md max-w-max mr-10">
+        <MobileMenu />
       </div>
-      <div className="col-span-2 h-50 bg-yellow-500 rounded-md ">02</div>
-      <div className="row-span-2 col-span-2 h-60 bg-purple-700 rounded-md">03</div>
+      <div>
+        <header className=" shadow-2xl rounded-md w-full">
+        <marquee>Тут вставляем текст бегущей строки</marquee>
+        </header>
+        <div className='mt-10 shadow-2xl rounded-md'>
+          <div className="bg-orange-400 w-40 h-60"> </div>
+        </div>
+      </div>
     </div>
   );
 }
